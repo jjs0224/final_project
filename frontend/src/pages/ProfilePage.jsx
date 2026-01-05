19253e106a5dd46776239ec869371df78b5cf6b7
binz <roaring94s@gmail.com>
2026-01-05 16:20:18 +0900

    pages and routes edit add

---
+++ frontend/src/pages/ProfilePage.jsx
@@ -0,0 +1,13 @@
+import './ProfilePage.css'
+
+function Profile(){
+    return(
+        <div>
+            <h1>
+                개인 Page 입니다.
+            </h1>
+        </div>
+    )
+};
+
+export default Profile;
---
+++ frontend/src/pages/SignupPage.jsx
@@ -0,0 +1,13 @@
+import './SignupPage.css';
+
+function SignupPage() {
+    return(
+        <div>
+            <h1>
+                회원가입 Page 입니다.
+            </h1>
+        </div>
+    )
+};
+
+export default SignupPage;
